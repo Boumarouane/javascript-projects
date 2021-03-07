0af8b04adc77a5e70b8770d7bb188805e2202a2a
Boumarouane <i.boumarouane@gmail.com>
2021-03-07 15:15:28 +0100

    icone rotate

--- dropdown/dropdown.js
+++ dropdown/dropdown.js
@@ -1,14 +1,17 @@
 const dropdown = document.querySelector(".dropdown");
 const btnDrop = document.querySelector(".bloc-top");
+const iconePlus = document.querySelector(".icone-plus");
 
 let toggleIndex = 0;
 
 btnDrop.addEventListener("click", () => {
   if (toggleIndex === 0) {
     dropdown.style.height = `${dropdown.scrollHeight}px`;
+    iconePlus.style.transform = "rotate(45deg)";
     toggleIndex++;
   } else {
     dropdown.style.height = `${btnDrop.scrollHeight}px`;
+    iconePlus.style.transform = "rotate(0deg)";
     toggleIndex--;
   }
 });
